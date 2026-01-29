372481b93aeda4058bd9dad6c29a6f71cbff7852
dipenrathapa <dipendatacentric@gmail.com>
2026-01-30 00:43:37 +0100

    as health endpoint edited, cd-cd health part need update too

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,8 @@
-#![allow(clippy::multiple_crate_versions)]
-#[allow(clippy::pedantic)]
+#![allow(clippy::pedantic)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::doc_markdown)]
+
 pub mod background;
 pub mod business;
 pub mod config;
@@ -9,7 +12,3 @@ pub mod retry;
 pub mod sensor;
 pub mod state;
 pub mod storage;
-
-// Re-export functions if needed
-pub use models::*;
-pub use sensor::*;
